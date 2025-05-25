ba415558b398be14ceccb721d38f68578482fd1b
Hitesh Kumar <hiteshagrawalmca10@gmail.com>
2025-05-25 14:25:45 +0530

    contact us form API added

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const connectDB = require('./config/db');
 
 const todoRoutes = require('./routes/todoRoutes');
 const authRoutes = require('./routes/authRoutes');
+const contactRoutes = require('./routes/contactRoutes');
 
 dotenv.config();
 connectDB();
@@ -15,6 +16,7 @@ app.use(express.json());
 
 app.use('/api/todos', todoRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/contact', contactRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
